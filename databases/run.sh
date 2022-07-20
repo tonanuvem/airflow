ae21a1cb14a81bdf67964eb135fc0661a4208915
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-07-20 18:30:59 -0300

    Create run.sh

---
+++ databases/run.sh
@@ -0,0 +1,22 @@
+# Liberando a porta no caso de rodar no Cloud9
+sudo service mysql stop
+
+docker-compose up -d
+
+#echo ""
+#echo "Aguardando a configuração do Debezium CDC (Change Data Capture)."
+#while [ "$(docker logs nifi_connect_1 2>&1 | grep "Finished starting connectors and tasks" | wc -l)" != "1" ]; do
+#  printf "."
+#  sleep 1
+#done
+echo ""
+echo "Config OK"
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - PostGres UI    : $IP:8070"
+echo " - Mongo UI       : $IP:8081"
+echo " - MySQL UI        : $IP:8082"
+echo " - Redis UI        : $IP:8083"
+echo ""
