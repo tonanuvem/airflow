e59ac35b23878542bb9fb6cfabba9a07824df1d3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-07 22:05:55 -0300

    Create run.sh

---
+++ jupyter/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Executando Jupyter Notebook para análise e transformação dos dados"
+
+docker run -it --name jupyter --rm -p 8888:8888 -v "${PWD}":/home/jovyan/work jupyter/datascience-notebook
+
+
+echo ""
+echo "Aguardando a configuração do Jupyter."
+
+while [ "$(docker logs jupyter | grep "http://127.0.0.1:8888/lab?token"| wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+
+echo ""
+echo ""
+echo "Senha de Bootstrap:"
+echo ""
+
+docker logs jupyter | grep "http://127.0.0.1:8888/lab?token"
