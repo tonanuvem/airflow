16cd2d3b32c0fa589c2c1a3179756544dc292e03
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-08 00:10:51 -0300

    Update run.sh

--- jupyter/run.sh
+++ jupyter/run.sh
@@ -6,7 +6,7 @@ dir=`pwd`; dir="$(dirname "$dir")"; echo $dir;
 
 #https://jupyter-server.readthedocs.io/en/latest/operators/public-server.html#preparing-a-hashed-password
 
-docker run -it --name jupyter --rm -p 8888:8888 -v "$dir":/home/jovyan/work -d jupyter/datascience-notebook \
+docker run -it --name jupyter --rm -p 8888:8888 -v "$dir":/home/jovyan/dir -v curso_transform.ipynb:/home/jovyan/curso_transform.ipynb -d jupyter/datascience-notebook \
     start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$cIQ7S1OapqyvWzmH636CsA$fh0xOdGwdwv6/cxW5Bqi2mPZuSZlG0zGLwcxl7Ulvac'
 
 echo ""
