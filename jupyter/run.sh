d23f03c4b6814d3ea310e927396bbd7c35082015
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-07 22:53:34 -0300

    Update run.sh

--- jupyter/run.sh
+++ jupyter/run.sh
@@ -10,5 +10,9 @@ docker run -it --name jupyter --rm -p 8888:8888 -v "$dir":/home/jovyan/work -d j
     start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$cIQ7S1OapqyvWzmH636CsA$fh0xOdGwdwv6/cxW5Bqi2mPZuSZlG0zGLwcxl7Ulvac'
 
 echo ""
+echo "URL de acesso:"
+echo ""
+echo http://$(curl -s checkip.amazonaws.com):8888
+echo ""
 echo "   Senha: admin"
 
