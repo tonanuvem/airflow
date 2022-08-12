9edac1e4d303fb95defba9592266c22b0306dda3
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-12 10:47:22 -0300

    Update build_images.sh

--- build_images.sh
+++ build_images.sh
@@ -1,2 +1,2 @@
 docker build -t tonanuvem/mysql:curso_db -f databases/Dockerfile.mysql.curso .
-docker build -t tonanuvem/airflow:etl -f Dockerfile/Dockerfile .
+docker build -t tonanuvem/airflow:etl -f Dockerfile/Dockerfile.tonanuvem.airflow .
