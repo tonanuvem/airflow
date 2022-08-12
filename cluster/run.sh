1bc518a2c28067874fe8a89cd1c6b70f98f06252
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-12 10:30:24 -0300

    Update run.sh

--- cluster/run.sh
+++ cluster/run.sh
@@ -2,4 +2,13 @@ docker stack deploy --compose-file stack.yml etl
 
 sleep 5
 
+echo ""
+echo ""
+
+docker stack ps etl --no-trunc
+
+echo "Aguardando 60 seg para verificar novamente:"
+echo ""
+
+sleep 60
 docker stack ps etl --no-trunc
