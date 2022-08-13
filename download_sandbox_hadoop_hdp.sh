fb72c676d84d628c4d07b8a04c763c08523d0427
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-13 16:15:25 -0300

    Update download_sandbox_hadoop_hdp.sh

--- download_sandbox_hadoop_hdp.sh
+++ download_sandbox_hadoop_hdp.sh
@@ -1,2 +1,2 @@
 wget https://archive.cloudera.com/hwx-sandbox/hdp/hdp-3.0.1/HDP_3.0.1_docker-deploy-scripts_18120587fc7fb.zip
-unzip HDP_3.0.1_docker-deploy-scripts_18120587fc7fb.zip
+unzip HDP_3.0.1_docker-deploy-scripts_18120587fc7fb.zip -d sandbox
