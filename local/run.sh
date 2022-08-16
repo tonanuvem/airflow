a91925a414e2370ba9c6c09ae7ee2b9fb7d9a1fe
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-16 08:25:06 -0300

    Update run.sh

--- local/run.sh
+++ local/run.sh
@@ -1,2 +1,15 @@
 docker-compose up -d
-docker logs local-airflow-1 | grep "Login with username: admin"
+echo ""
+echo "Aguardando a configuração."
+while [ "$(docker logs local-airflow-1 2>&1 | grep ""Login with username: admin"" | wc -l)" != "1" ]; do
+  printf "."
+  sleep 1
+done
+docker exec -ti local-airflow-1 airflow users create --role Admin --username fiap --email fiap --firstname fiap --lastname fiap --password fiap
+echo "Config OK"
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto: (login = fiap, password = fiap)"
+echo ""
+echo " - AIRFLOW         : $IP:18080"
+echo ""
