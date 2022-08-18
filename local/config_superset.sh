f16754d0d744cdf69d9795f33b6e614391974569
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-08-18 17:07:41 -0300

    Update config_superset.sh

--- local/config_superset.sh
+++ local/config_superset.sh
@@ -20,8 +20,9 @@ docker exec -it superset_app superset db upgrade
 # Setup roles
 docker exec -it superset_app superset init
 
-#echo "Load Dashboards"
-#docker exec -it superset_app superset import-dashboards -p /app/superset_home/superset/dashboard_ETL.zip
+echo "Loading Dashboard:"
+echo 
+docker exec -it superset_app superset import-dashboards -p /app/superset_home/superset/dashboard_ETL.zip
 
 echo ""
 echo ""
