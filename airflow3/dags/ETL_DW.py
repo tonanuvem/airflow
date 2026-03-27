e3fa8a81f871794cbc4df3da3bc06c81fe8d4459
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-03-27 17:05:25 -0300

    Update ETL_DW.py

--- airflow3/dags/ETL_DW.py
+++ airflow3/dags/ETL_DW.py
@@ -21,6 +21,7 @@ def extrair_dados():
     from pymysql import connect
     import pandas.io.sql as sql
 
+    # conecta ao mysql via python
     con = connect(
         user="admin",
         password="admin",
@@ -28,8 +29,10 @@ def extrair_dados():
         database="fiap"
     )
 
+    # le os dados
     df = sql.read_sql('SELECT * FROM curso', con)
 
+    # exportar dados para a próxima task
     print(df.info())
     df.to_csv(CSV_FILE, encoding='utf-8-sig', index=False)
 
@@ -40,14 +43,19 @@ def extrair_dados():
 def clean_dados(input_file: str):
     """
     Limpeza dos dados
+    Nesta etapa, podemos limpar por ex colunas e dados nulos, formatos de datas invalidas, etc.
     """
     import pandas as pd
 
     df = pd.read_csv(input_file)
 
+    # 1) eliminar matrículas duplicadas
     df = df.drop_duplicates(subset='MATRICULA', keep="first")
+
+    # 2) resolver o que fazer com os valores nulos da materia 4 : trocar NaN por 0
     df["NOTA_MAT_4"] = df["NOTA_MAT_4"].fillna(0)
 
+    # exportar dados para a próxima task
     print(df.info())
     df.to_csv(CSV_CLEAN, encoding='utf-8-sig', index=False)
 
@@ -58,17 +66,20 @@ def clean_dados(input_file: str):
 def transformar_dados(input_file: str):
     """
     Transformação dos dados
+    Responsável por agregar e transformar dados: podemos, por ex, criar novas colunas.
     """
     import pandas as pd
     import numpy as np
 
     df = pd.read_csv(input_file)
 
+    # 3) inserir coluna descritiva sobre os alunos que falam ingles, ajustando valores nulos
     df["INGLES"] = df["INGLES"].fillna(-1)
     conditions = [df['INGLES'] > 0, df['INGLES'] == 0, df['INGLES'] < 0]
     choices = ['SIM', 'NÃO', 'SEM RESPOSTA']
     df['INGLES_DESC'] = np.select(conditions, choices)
 
+    # 4) eliminar nota ZERO de alunos sem reprovação (ainda não cursaram as matérias 1, 2, 3, 4)
     # Condições matérias
     def gerar_status(nota, reprova):
         return np.select(
@@ -79,12 +90,12 @@ def transformar_dados(input_file: str):
             ],
             ['APROVADO', 'REPROVADO', 'AINDA NAO CURSOU']
         )
-
     df['CURSOU_MAT1_DESC'] = gerar_status(df['NOTA_MAT_1'], df['REPROVACOES_MAT_1'])
     df['CURSOU_MAT2_DESC'] = gerar_status(df['NOTA_MAT_2'], df['REPROVACOES_MAT_2'])
     df['CURSOU_MAT3_DESC'] = gerar_status(df['NOTA_MAT_3'], df['REPROVACOES_MAT_3'])
     df['CURSOU_MAT4_DESC'] = gerar_status(df['NOTA_MAT_4'], df['REPROVACOES_MAT_4'])
 
+    # exportar dados para a próxima task
     print(df.info())
     df.to_csv(CSV_TRANSFORM, encoding='utf-8-sig', index=False)
 
@@ -109,7 +120,7 @@ def carregar_para_dw(input_file: str):
     print('Carga concluída com sucesso')
 
 
-# DAG
+# instanciar fluxo do DAG e suas configs
 with DAG(
     dag_id=DAG_ID,
     start_date=datetime(2024, 1, 1),
