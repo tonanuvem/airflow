c1f07dda64da506061c678ddd08edfdaa7e9b82b
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-03-27 17:55:54 -0300

    Update recarregar_dag.sh

--- airflow3/recarregar_dag.sh
+++ airflow3/recarregar_dag.sh
@@ -1,1 +1,14 @@
 docker exec airflow airflow dags delete ETL_DW --yes 
+
+echo "Aguardando 30 seg, enquanto o Airflow importa de volta a DAG..."
+while ! docker exec -i airflow airflow dags show ETL_DW > /dev/null 2>&1; do
+  echo "⏳ aguardando DAG..."
+  sleep 5
+done
+
+docker exec -i airflow airflow dags list-import-errors | grep ETL_DW && {
+  echo "❌ erro na DAG"
+  exit 1
+}
+
+echo "✅ DAG pronta"
